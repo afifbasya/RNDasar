7168fd9ecc454342eaa19d20137a5cd094f42e0d
Afif Uddin <afifuddin@Afifs-MacBook-Air.local>
2023-05-09 21:22:22 +0700

    initial

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Avatar from './assets/avatar.jpeg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Image
+        source={{ uri: "https://avatars.githubusercontent.com/u/22425167?v=4" }}
+        style={styles.gambar}
+      /> */}
+      <Image source={Avatar} style={styles.gambar} />
+
+      <Text style={styles.nama}>Muhammad Afifuddin</Text>
+      <Text>Saya sebagai Programmer yang Multitalenta</Text>
+
+      <View style={styles.pekerjaan}>
+        <Text>Pekerjaaan :</Text>
+        <View style={styles.item}>
+          <Text>Front End Developer</Text>
+          <Text>di Bussan Auto Finance</Text>
+        </View>
+        <View style={styles.item}>
+          <Text>Mobile Developer</Text>
+          <Text>di Prospark</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#93CAED',
+    padding: 30
+    // margin: 30
   },
+  gambar: {
+    width: 150,
+    height: 150,
+    borderRadius: 20
+  },
+  nama: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  pekerjaan: {
+    marginTop: 20
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
+
